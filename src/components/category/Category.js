a113e8b88cbdf9cca76c02e83e2f590081f3ce37
KROomoO <parksungik00@naver.com>
2023-05-12 22:01:56 +0900

    add : axios event

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -5,8 +5,10 @@ import GuCategory from "./GuCategory";
 
 function Category() {
     const [guname, setGuname] = useState();
+    const [selectGuname, setSelectGuname] = useState("");
     const [checkedFree, setCheckedFree] = useState(false);
 
+    // load guname list
     useEffect(() => {
         Axios.post("/api/list")
             .then((response) => {
@@ -17,12 +19,48 @@ function Category() {
             });
     }, []);
 
-    // useEffect(() => {
-    //     Axios.post("/api/list/free").then((response) => {
-    //         console.log(response.data);
-    //     });
-    // }, [checkedFree]);
+    // load selected guname list
+    useEffect(() => {
+        if (selectGuname !== "") {
+            Axios.post("/api/list/guname", {
+                guname: selectGuname,
+                checkedFree: checkedFree,
+            })
+                .then((response) => {
+                    console.log(response.data);
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+        }
+    }, [checkedFree, selectGuname]);
 
+    // toggle selected category underline effect Promise
+    const selectCategoryPromise = (props) => {
+        return new Promise(function (resolve, reject) {
+            const parent = props.target.parentNode;
+
+            parent.childNodes.forEach((item) => {
+                item.classList.remove("selected");
+            });
+
+            resolve(props.target);
+        });
+    };
+
+    // toggle selected category underline effect
+    const handleSelectCategory = (props) => {
+        selectCategoryPromise(props).then((item) => {
+            item.classList.add("selected");
+        });
+    };
+
+    // selectGuname Axios Trigger
+    const handleClickGuname = (selectGu) => {
+        setSelectGuname(selectGu);
+    };
+
+    // set useState checkedFree handler
     const handleCheckedFree = (event) => {
         setCheckedFree(event.target.checked);
     };
@@ -31,7 +69,9 @@ function Category() {
             <div className="main_type_container">
                 <div className="main_type">
                     <ul className="main_category">
-                        <li>자치구</li>
+                        <li className="selected" onClick={handleSelectCategory}>
+                            자치구
+                        </li>
                         {/* <li>추후 추가 카테고리</li> */}
                     </ul>
                     <div>
@@ -48,9 +88,17 @@ function Category() {
                     <ul className="middle_category">
                         {guname &&
                             guname.map((item, index) => {
-                                return <GuCategory key={index} {...item} />;
+                                return (
+                                    <GuCategory
+                                        key={index}
+                                        {...item}
+                                        handleSelectCategory={
+                                            handleSelectCategory
+                                        }
+                                        handleClickGuname={handleClickGuname}
+                                    />
+                                );
                             })}
-                        {useEffect(() => {}, [])}
                     </ul>
                 </div>
             </div>
--- src/components/category/GuCategory.js
+++ src/components/category/GuCategory.js
@@ -1,68 +1,13 @@
-import Axios from "axios";
-import { useState, useEffect } from "react";
-
-function GuCategory({ guname }) {
-    const [selectCategory, setSelectCategory] = useState();
-    const [selectGuname, setSelectGuname] = useState("");
-
-    useEffect(() => {
-        document.querySelector(".main_category").childNodes.forEach((item) => {
-            item.addEventListener("click", handleSelectCategory);
-        });
-        document
-            .querySelector(".middle_category")
-            .childNodes.forEach((item) => {
-                item.addEventListener("click", handleSelectCategory);
-            });
-    }, []);
-
-    useEffect(() => {
-        if (selectCategory !== undefined) {
-            selectCategory.classList.add("selected");
-        }
-    }, [selectCategory]);
-
-    useEffect(() => {
-        if (selectGuname !== "") {
-            Axios.post("/api/list/guname", { guname: selectGuname })
-                .then((response) => {
-                    console.log(response.data);
-                })
-                .catch((err) => {
-                    console.error(err);
-                })
-                .finally(() => {
-                    setSelectGuname("");
-                });
-        }
-    }, [selectGuname]);
-
-    const selectCategoryPromise = (props) => {
-        return new Promise(function (resolve, reject) {
-            const parent = props.target.parentNode;
-
-            parent.childNodes.forEach((item) => {
-                item.classList.remove("selected");
-            });
-
-            resolve(props.target);
-        });
-    };
-
-    const handleSelectCategory = (props) => {
-        selectCategoryPromise(props).then((item) => {
-            setSelectCategory(item);
-        });
-    };
-
-    const handleClickGuname = (selectGu) => {
-        setSelectGuname(selectGu);
-    };
-
+function GuCategory({ guname, handleSelectCategory, handleClickGuname }) {
     return (
         <>
             {guname !== "None" ? (
-                <li value={guname} onClick={() => handleClickGuname(guname)}>
+                <li
+                    onClick={(e) => {
+                        handleSelectCategory(e);
+                        handleClickGuname(e.target.innerText);
+                    }}
+                >
                     {guname}
                 </li>
             ) : null}
