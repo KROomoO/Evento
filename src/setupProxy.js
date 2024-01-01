a26b2ce9a35e9beac58d5f08cb53eda33ff88901
KROomoO <pwkc00@gmail.com>
2024-01-02 00:48:24 +0900

    change

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,8 +3,8 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = (app) => {
     app.use(
         createProxyMiddleware("/api", {
-            target: "http://3.35.218.75:4000/",
-            // target: "http://localhost:4000",
+            // target: "http://3.35.218.75:4000/",
+            target: "http://localhost:4000",
             changeOrigin: true,
         })
     );
